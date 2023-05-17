ff849c157692d11cdbcb8d74b1ef42d3fe614dd8
luiz-paulo-frutuoso <luiz.frutuoso@sptech.school>
2023-05-17 19:37:29 -0300

    teste de objetos json

---
+++ site/funcoes.js
@@ -0,0 +1,12 @@
+var objeto_teste = {
+    subarea: ['desenvolvimento','qualidade','analise'],
+    ferramentas_desenvolvimento:['Vs Code','JetBrains','Android Studio'],
+    ferramentas_qualidade:['teste unitario','testes de mesa'],
+    ferramentas_analise:['draw.io','visio']
+}
+var contador_votos = 0;
+
+function votar() {
+    contador_votos ++;
+    console.log(contador_votos)
+}
